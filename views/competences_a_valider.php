e2ffeb7efb379a4b580c3ac561305fab6107cba9
Strauss72 <loicguen@gmail.com>
2016-01-13 14:53:30 +0100

    ajouts icones, barres de progression et agrandissement de la zone clickable des panels

--- views/competences_a_valider.php
+++ views/competences_a_valider.php
@@ -5,30 +5,37 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">
-                        <a role="button" data-toggle=collapse href="#collapseOne" aria-controls="collapseOne" aria-expanded="false">
+                        <a class="btn-block" role="button" data-toggle=collapse href="#collapseOne" aria-controls="collapseOne" aria-expanded="false">
                             Compétence 1
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%"><span class="sr-only">20% Complete</span></div>
+                            </div>
                         </a>
                     </h3>
                 </div>
                 <div id="collapseOne" class="collapse">
                     <div class="list-group">
-                        <button class="list-group-item" >sous-compétence 1-1</button>
-                        <button class="list-group-item" >sous-compétence 1-2</button>
+                        <button class="list-group-item"><span class="glyphicon glyphicon-ok pull-right"></span>sous-compétence 1-1</button>
+                        <button class="list-group-item"><span class="glyphicon glyphicon-ok pull-right"></span>sous-compétence 1-2</button>
+                        <button class="list-group-item"><span class="glyphicon glyphicon-ok pull-right"></span>sous-compétence 1-3</button>
                     </div>
                 </div>
             </div>
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">
-                        <a role="button" data-toggle=collapse href="#collapseTwo" aria-controls="collapseTwo" aria-expanded="false">
+                        <a class="btn-block" role="button" data-toggle=collapse href="#collapseTwo" aria-controls="collapseTwo" aria-expanded="false">
                             Compétence 2
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"><span class="sr-only">100% Complete</span></div>
+                            </div>
                         </a>
                     </h3>
                 </div>
                 <div id="collapseTwo" class="collapse">
                     <div class="list-group">
-                        <button class="list-group-item" >sous-compétence 2-1</button>
-                        <button class="list-group-item" >sous-compétence 2-2</button>
+                        <button class="list-group-item"><span class="glyphicon glyphicon-ok pull-right"></span>sous-compétence 2-1</button>
+                        <button class="list-group-item"><span class="glyphicon glyphicon-ok pull-right"></span>sous-compétence 2-2</button>
                     </div>
                 </div>
             </div>
