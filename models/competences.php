384f76040019c88de1ddd3546c3c907262e789d9
ronanyzeux <ronan.yzeux@gmail.com>
2016-01-13 14:42:03 +0100

    ajout du fichier competences.php

---
+++ models/competences.php
@@ -0,0 +1,27 @@
+<?php
+
+function competencePreprofessionnelles(){
+	$competences = array();
+	$query = "Select nomCompetence Where idCategorie = 2";
+	$result = mysql_result($query);
+
+	while($row = mysql_fetch_array($result)){
+		$ompetences[] = $row[0];
+	}
+
+	return $competences
+}
+
+function competencesDisciplinaires($id){
+	$competences = array();
+	$query = "Select nomCompetence Where idCategorie = 1 and idMention = '$id' ";
+	$result = mysql_result($query);
+
+	while($row = mysql_fetch_array($result)){
+		$ompetences[] = $row[0];
+	}
+
+	return $competences
+}
+
+?>
