055e482d83804c6acbc8179049853ada38562219
CorentinDelorme <Corentin.Delorme.Etu@univ-lemans.fr>
2016-01-19 11:23:15 +0100

    Fix du footer lorsque la page est plus grande que la hauteur de l'écran. Début de l'arbre des compétences.

--- views/arbre_competences.php
+++ views/arbre_competences.php
@@ -1,8 +1,95 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        Arbre des compétences
+        Arbre des compétences validées
     </div>
-    <div class="panel-body">
 
+    <div class="panel-group" id="arbre_competences" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="arbre_competences_headingOne">
+                <h3 class="panel-title">
+                    <a role="button" data-toggle="collapse" data-parent="#arbre_competences" href="#arbre_competences_collapseOne" aria-expanded="true" aria-controls="arbre_competences_collapseOne">
+                    Compétences disciplinaires
+                </a>
+                </h3>
+            </div>
+            <div id="arbre_competences_collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <table class="table table-bordered table-hover table-condensed">
+                    <thead>
+                        <tr>
+                            <th>Compétence</th>
+                            <th>Date de validation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Test</td>
+                            <td>14/12/2015</td>
+                        </tr>
+                        <tr>
+                            <td>Test</td>
+                            <td>12/11/2015</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="arbre_competences_headingTwo">
+                <h3 class="panel-title">
+                    <a role="button" data-toggle="collapse" data-parent="#arbre_competences" href="#arbre_competences_collapseTwo" aria-expanded="true" aria-controls="arbre_competences_collapseTwo">
+                Compétences préprofessionnelles
+            </a></h3>
+            </div>
+            <div id="arbre_competences_collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <table class="table table-bordered table-hover table-condensed">
+                    <thead>
+                        <tr>
+                            <th>Compétence</th>
+                            <th>Date de validation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Situer son rôle et sa mission au sein d'une organisation pour s'adapter et prendre des initiatives</td>
+                            <td>17/01/2016</td>
+                        </tr>
+                        <tr>
+                            <td>Identifier le processus de production, de diffusion et de valorisation des savoirs</td>
+                            <td>12/01/2016</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="arbre_competences_headingThree">
+                <h3 class="panel-title">
+                    <a role="button" data-toggle="collapse" data-parent="#arbre_competences" href="#arbre_competences_collapseThree" aria-expanded="true" aria-controls="arbre_competences_collapseThree">
+                Compétences transversales et linguistiques
+            </a></h3>
+            </div>
+            <div id="arbre_competences_collapseThree" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <table class="table table-bordered table-hover table-condensed">
+                    <thead>
+                        <tr>
+                            <th>Compétence</th>
+                            <th>Date de validation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Utiliser les outils numériques de référence et les règles de sécurité informatique pour acquérir, traiter, produire et diffuser de l’information ainsi que pour collaborer en interne et en externe</td>
+                            <td>11/01/2016</td>
+                        </tr>
+                        <tr>
+                            <td>Identifier et sélectionner diverses ressources spécialisées pour documenter un sujet</td>
+                            <td>05/01/2016</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
