ed28d57543f6f4549e3f90cda9ef3860cf6d20d0
Strauss72 <loicguen@gmail.com>
2016-01-22 14:43:06 +0100

    Suppression panels competences

--- js/validationcompetences.js
+++ js/validationcompetences.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 function validation(button){
-    var comp  = document.getElementById("progress1");
-    comp.style.width="50%";
     button.style.background="#5cb85c";
 }
+
+function afficherCompetence(id){}
--- views/competences_a_valider.php
+++ views/competences_a_valider.php
@@ -1,56 +1,23 @@
 <div class="panel panel-default">
-    <div class="panel-heading">Compétences à valider</div>
+    <div class="panel-heading">Compétences à valider
+    </div>
     <div class="panel-body">
-        <div class="panel-group" id="liste_competences">
-            <div class="panel panel-default">
-                <a class="btn-block panel-heading" role="button" data-parent="#liste_competences" data-toggle="collapse" href="#collapseOne" aria-controls="collapseOne" aria-expanded="false">
-                    <h3 class="panel-title">
-                            Compétence 1
-                    </h3>
-                    <div class="progress">
-                        <div  id="progress1"class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" >50%
-                        </div>
-                    </div>
-                </a>
-                <div id="collapseOne" class="collapse">
-                    <div class="list-group">
-                        <div class="list-group-item btn"onclick="validation(this)">
-                            <label >
-                                sous-compétence 1-1
-                            </label>
-                        </div>
-                        <div class="list-group-item btn"onclick="validation(this)">
-                            <label >
-                                sous-compétence 1-2
-                            </label>
-                        </div>
-                    </div>
-                </div>
+        <div class="list-group">
+            <div class="list-group-item btn" onclick="validation(this)">
+                <label >                                sous-compétence 1-1
+                </label>
             </div>
-            <div class="panel panel-default">
-                <a class="btn-block panel-heading" role="button" data-parent="#liste_competences" data-toggle="collapse" href="#collapseTwo" aria-controls="collapseTwo" aria-expanded="false">
-                    <h3 class="panel-title">
-                            Compétence 2
-                    </h3>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">100%
-                        </div>
-                    </div>
-                </a>
-                <div id="collapseTwo" class="collapse">
-                    <div class="list-group">
-                        <div class="list-group-item btn"onclick="validation(this)">
-                            <label >
-                                sous-compétence 2-1
-                            </label>
-                        </div>
-                        <div class="list-group-item btn"onclick="validation(this)">
-                            <label >
-                                sous-compétence 2-2
-                            </label>
-                        </div>
-                    </div>
-                </div>
+            <div class="list-group-item btn" onclick="validation(this)">
+                <label >                                sous-compétence 1-2
+                </label>
+            </div>
+            <div class="list-group-item btn" onclick="validation(this)">
+                <label >                                sous-compétence 2-1
+                </label>
+            </div>
+            <div class="list-group-item btn" onclick="validation(this)">
+                <label >                                sous-compétence 2-2
+                </label>
             </div>
         </div>
     </div>
