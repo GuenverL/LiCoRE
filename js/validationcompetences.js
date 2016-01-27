85458f012a829140e55ea1eaf31f5209052d28e1
Strauss72 <loicguen@gmail.com>
2016-01-27 16:58:33 +0100

    ajout icone de validation

--- js/validationcompetences.js
+++ js/validationcompetences.js
@@ -1,13 +1,19 @@
+
 function validation(button,id) {
+    id="idComp"+id
+    span=document.getElementById(id)
+
     if(button.className == "list-group-item"){
         button.style.background = "#5cb85c";
         button.className += " validated"
+        span.className="glyphicon glyphicon-remove"
+
     }else{
         button.style.background = "#ffffff";
-        button.className = "list-group-item"
+        button.className = "list-group-item";
+        span.className="glyphicon glyphicon-ok"
     }
 }
-
 
 function afficherCompetence(id) {
     $.getJSON('api/competences.php', {
@@ -18,9 +24,11 @@ function afficherCompetence(id) {
             $("#competences-a-valider").empty();
             for (competence of competences) {
 
-                $("#competences-a-valider").append('<div class="list-group-item" onclick="validation(this,' + competence.id + ')">' +
-                    '<label>' + competence.nom + '</label>' +
-                    '</div>');
+                $("#competences-a-valider").append('<div class="list-group-item" onclick="validation(this,' + competence.id + ')">'+
+                    '<div class="media"><div class="media-body">' +
+                        competence.nom +
+                    '</div><div class="media-right media-middle"><span id="idComp'+competence.id+'" class="glyphicon glyphicon-ok" aria-hidden="true"><span></div></div>' +
+                '</div>');
             }
         }
     );
