8ec550ec3c133e37a4fc45eeded90c4946a52e1d
Strauss72 <loicguen@gmail.com>
2016-01-29 15:50:39 +0100

    ajout nom categorie a la colonne 2

--- js/validationcompetences.js
+++ js/validationcompetences.js
@@ -21,13 +21,14 @@ function afficherCompetence(id) {
             idPere: id
         },
         function(competences) {
-            $("#competences-a-valider").empty();
+            $("#panel-body-competences").empty();
+            $("#panel-body-competences").append('nomCompetence : <div id="competences-a-valider" class="list-group"></div>');
             for (competence of competences) {
 
                 $("#competences-a-valider").append('<div class="list-group-item" onclick="validation(this,' + competence.id + ')">'+
                     '<div class="media"><div class="media-body">' +
                         competence.nom +
-                    '</div><div class="media-right media-middle"><span id="idComp'+competence.id+'" class="glyphicon glyphicon-ok" aria-hidden="true"><span></div></div>' +
+                    '</div><div class="media-right media-middle"><span id="idComp'+competence.id+'" class="glyphicon glyphicon-ok" aria-hidden="true"></span></div></div>' +
                 '</div>');
             }
         }).fail( function(competences, textStatus, error) {
--- views/competences_a_valider.php
+++ views/competences_a_valider.php
@@ -1,7 +1,7 @@
 <div class="panel panel-default">
     <div class="panel-heading">Compétences à valider
     </div>
-    <div class="panel-body">
+    <div id="panel-body-competences" class="panel-body">
         <div id="competences-a-valider" class="list-group"></div>
     </div>
 </div>
