a6b73847f6cc847d5eea5df611e5bd49d56e52a9
CorentinDelorme <Corentin.Delorme.Etu@univ-lemans.fr>
2016-02-02 16:28:58 +0100

    Fix des include dans api/competences.php

--- api/competences.php
+++ api/competences.php
@@ -1,4 +1,8 @@
 <?php
+/* XXX */
+define('DOC_ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].'/'.'Projet_Licore');
+include_once(DOC_ROOT_PATH . '/models/competences.php');
+include_once(DOC_ROOT_PATH . '/models/connexion_sql.php');
 
 header('Content-Type: application/json; charset=utf-8');
 $json = NULL;
