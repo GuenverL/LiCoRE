23b635136a13483e2ce59e2d2034fc0e9c7bd9d4
Strauss72 <loicguen@gmail.com>
2016-02-02 14:47:12 +0100

    IHM validation/invalidation competences
    Intégration dans l'interface de la validation et l'invalidation des
    competences

--- js/validationcompetences.js
+++ js/validationcompetences.js
@@ -1,37 +1,53 @@
 
+
 function validation(button,id) {
-    id="idComp"+id
-    span=document.getElementById(id)
+    idCompetence="idComp"+id
+    span=document.getElementById(idCompetence)
 
     if(button.className == "list-group-item"){
         button.style.background = "#d9ffd9";
-        button.className += " validated"
-        span.className="glyphicon glyphicon-remove"
-
+        button.className += " validated";
+        span.className="glyphicon glyphicon-remove";
+        validerCompetence(id);
     }else{
         button.style.background = "#ffffff";
         button.className = "list-group-item";
-        span.className="glyphicon glyphicon-ok"
+        span.className="glyphicon glyphicon-ok";
+        invaliderCompetence(id);
     }
 }
 
-var lienPrecedent = null;
-function afficherCompetence(lien,id) {
+function validerCompetence(id){
+    $.getJSON('api/competences.php', {
+            type: 'validation',
+            idCompetence: id
+        });
+}
 
-    if(lienPrecedent != null) {
+function invaliderCompetence(id){
+    $.getJSON('api/competences.php',{
+            type: 'invalidation',
+            idCompetence: id
+        });
+}
+
+var lienPrecedent = null;
+function afficherCompetence(lien,id){
+
+    if(lienPrecedent != null){
         lienPrecedent.className = "text-default";
     }
     lienPrecedent = lien;
     lienPrecedent.className = "text-selected";
 
-    $.getJSON('api/competences.php', {
+    $.getJSON('api/competences.php',{
             type: 'sousCompetences',
             idPere: id
         },
-        function(competences) {
+        function(competences){
             $("#panel-body-competences").empty();
             $("#panel-body-competences").append('<div class="list-group-item" style="background-color: #81c0c4">'+lien.children[0].innerHTML+'</div> <div id="competences-a-valider" class="list-group"></div>');
-            for (competence of competences) {
+            for (competence of competences){
                 if(competence.valide == true){
                     $("#competences-a-valider").append('<div class="list-group-item validated" onclick="validation(this,' + competence.id + ')" style="background-color: #d9ffd9">'+
                         '<div class="media"><div class="media-body">' +
@@ -46,7 +62,7 @@ function afficherCompetence(lien,id) {
                     '</div>');
                 }
             }
-        }).fail( function(competences, textStatus, error) {
+        }).fail( function(competences, textStatus, error){
             console.error("getJSON failed, status: " + textStatus + ", error: "+error)
         });
 }
