12abecfa19c89b4cf405c1abbab987498e10e6ac
CorentinDelorme <Corentin.Delorme.Etu@univ-lemans.fr>
2016-02-26 13:58:37 +0100

    Mise à jour des logos du pied de page.

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,8 +1,9 @@
 <footer class="navbar navbar-inverse vertical-center">
     <div class="container">
         <p class="text-center">
-            <img src="./images/logo_prn.png" alt="" />
-            <img src="./images/logo_um.png" alt="" />
+            <a href="http://www.univ-lemans.fr/"><img src="./images/logo_um.png" alt="Logo Université du Maine" /></a>
+            <a href="http://u-bretagneloire.fr/"><img src="./images/logo_ubl.png" alt="Logo Université Bretagne Loire" /></a>
+            <a href="http://www.paysdelaloire.fr/"><img src="./images/logo_pays_de_la_loire.png" alt="Logo Pays de la Loire" /></a>
         </p>
     </div>
 </footer>
