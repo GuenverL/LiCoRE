41159a47dde31e434401553188b5e2bd8227a8de
ronanyzeux <ronan.yzeux@gmail.com>
2016-02-26 14:13:25 +0100

    ajout du champ visible dans la table compétence de la bdd

--- Bdd/LicoreBdd.sql
+++ Bdd/LicoreBdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 09 Février 2016 à 14:48
+-- Généré le :  Ven 26 Février 2016 à 14:11
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -30,43 +30,44 @@ CREATE TABLE IF NOT EXISTS `competence` (
   `idCompetence` int(11) NOT NULL AUTO_INCREMENT,
   `nomCompetence` varchar(250) NOT NULL,
   `idPereCompetence` int(11) DEFAULT NULL,
+  `visible` tinyint(1) NOT NULL,
   PRIMARY KEY (`idCompetence`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=29 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
 
 --
 -- Contenu de la table `competence`
 --
 
-INSERT INTO `competence` (`idCompetence`, `nomCompetence`, `idPereCompetence`) VALUES
-(1, 'Situer son rôle et sa mission au sein d''une organisation pour s''adapter et prendre des initiatives', 28),
-(2, 'Identifier le processus de production, de diffusion et de valorisation des savoirs', 28),
-(3, 'Respecter les principes d’éthique, de déontologie et de responsabilité environnementale', 28),
-(4, 'Travailler en équipe autant qu’en autonomie et responsabilité au service d’un projet', 28),
-(5, 'Identifier et situer les champs professionnels potentiellement en relation avec les acquis de la mention ainsi que les parcours possibles pour y accéder', 28),
-(6, 'Caractériser et valoriser son identité, ses compétences et son projet professionnel en fonction d’un contexte', 28),
-(7, 'Se mettre en recul d’une situation, s’auto évaluer et se remettre en question pour apprendre', 28),
-(8, 'Utiliser les outils numériques de référence et les règles de sécurité informatique pour acquérir, traiter, produire et diffuser de l’information ainsi que pour collaborer en interne et en externe', 29),
-(9, 'Identifier et sélectionner diverses ressources spécialisées pour documenter un sujet', 29),
-(10, 'Analyser et synthétiser des données en vue de leur exploitation', 29),
-(11, 'Développer une argumentation avec esprit critique', 29),
-(12, 'Se servir aisément des différents registres d’expression écrite et orale de la langue française', 29),
-(13, 'Se servir aisément de la compréhension et de l’expression écrites et orales dans au moins une langue vivante étrangère', 29),
-(14, 'Identifier le rôle et le champ d’application des sciences pour l’ingénieur dans tous les\r\nsecteurs : milieux naturels, milieux industriels, transports, environnements urbains, etc', 26),
-(15, 'Valider un modèle par comparaison de ses prévisions aux résultats expérimentaux et apprécier ses limites de validité', 26),
-(16, 'Identifier les principales familles de matériaux et leurs propriétés', 26),
-(17, 'Mobiliser les outils mathématiques nécessaires à la modélisation', 26),
-(18, 'Mobiliser des concepts en mathématiques, en physique, en chimie, en thermodynamique, afin d’aborder des problèmes spécifiques aux différents domaines industriels', 26),
-(19, 'Estimer les ordres de grandeur et manipuler correctement les unités', 26),
-(20, 'Intégrer une vision correcte de l’espace et de ses représentations', 26),
-(21, 'Isoler un système', 26),
-(22, 'Mettre en œuvre des techniques d’algorithmique et de programmation, notamment pour développer des applications simples d’acquisition et de traitements de données', 26),
-(23, 'Droit', 27),
-(24, 'Économie', 27),
-(25, 'Histoire', 27),
-(26, 'Sciences pour l''ingénieur\r\n', 27),
-(27, 'Disciplinaires', NULL),
-(28, 'Préprofessionnelles', NULL),
-(29, 'Transversales et linguistiques', NULL);
+INSERT INTO `competence` (`idCompetence`, `nomCompetence`, `idPereCompetence`, `visible`) VALUES
+(1, 'Situer son rôle et sa mission au sein d''une organisation pour s''adapter et prendre des initiatives', 28, 1),
+(2, 'Identifier le processus de production, de diffusion et de valorisation des savoirs', 28, 1),
+(3, 'Respecter les principes d’éthique, de déontologie et de responsabilité environnementale', 28, 1),
+(4, 'Travailler en équipe autant qu’en autonomie et responsabilité au service d’un projet', 28, 1),
+(5, 'Identifier et situer les champs professionnels potentiellement en relation avec les acquis de la mention ainsi que les parcours possibles pour y accéder', 28, 1),
+(6, 'Caractériser et valoriser son identité, ses compétences et son projet professionnel en fonction d’un contexte', 28, 1),
+(7, 'Se mettre en recul d’une situation, s’auto évaluer et se remettre en question pour apprendre', 28, 1),
+(8, 'Utiliser les outils numériques de référence et les règles de sécurité informatique pour acquérir, traiter, produire et diffuser de l’information ainsi que pour collaborer en interne et en externe', 29, 1),
+(9, 'Identifier et sélectionner diverses ressources spécialisées pour documenter un sujet', 29, 1),
+(10, 'Analyser et synthétiser des données en vue de leur exploitation', 29, 1),
+(11, 'Développer une argumentation avec esprit critique', 29, 1),
+(12, 'Se servir aisément des différents registres d’expression écrite et orale de la langue française', 29, 1),
+(13, 'Se servir aisément de la compréhension et de l’expression écrites et orales dans au moins une langue vivante étrangère', 29, 1),
+(14, 'Identifier le rôle et le champ d’application des sciences pour l’ingénieur dans tous les\r\nsecteurs : milieux naturels, milieux industriels, transports, environnements urbains, etc', 26, 1),
+(15, 'Valider un modèle par comparaison de ses prévisions aux résultats expérimentaux et apprécier ses limites de validité', 26, 1),
+(16, 'Identifier les principales familles de matériaux et leurs propriétés', 26, 1),
+(17, 'Mobiliser les outils mathématiques nécessaires à la modélisation', 26, 1),
+(18, 'Mobiliser des concepts en mathématiques, en physique, en chimie, en thermodynamique, afin d’aborder des problèmes spécifiques aux différents domaines industriels', 26, 1),
+(19, 'Estimer les ordres de grandeur et manipuler correctement les unités', 26, 1),
+(20, 'Intégrer une vision correcte de l’espace et de ses représentations', 26, 1),
+(21, 'Isoler un système', 26, 1),
+(22, 'Mettre en œuvre des techniques d’algorithmique et de programmation, notamment pour développer des applications simples d’acquisition et de traitements de données', 26, 1),
+(23, 'Droit', 27, 1),
+(24, 'Économie', 27, 1),
+(25, 'Histoire', 27, 1),
+(26, 'Sciences pour l''ingénieur\r\n', 27, 1),
+(27, 'Disciplinaires', NULL, 1),
+(28, 'Préprofessionnelles', NULL, 1),
+(29, 'Transversales et linguistiques', NULL, 1);
 
 -- --------------------------------------------------------
 
