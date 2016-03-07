60730fc9db05b8a532937fb84042246c236a6789
Strauss72 <loicguen@gmail.com>
2016-03-07 10:38:28 +0100

    maj export XML

--- js/export-competences.js
+++ js/export-competences.js
@@ -1,25 +1,16 @@
 // JavaScript Document
 
-/*window.onload = function() {
+function exportArbre(){
     var arbre = '<?xml version = "1.0" encoding="UTF-8" standalone="yes" ?>\n<arbre>\n';
-       $.getJSON('api/competences.php',{
-                    type: 'getCompetencesValides'
-                },
-                function(competences){
-                  for (var i = 0, len = competences.length; i < len; ++i) {
-                    var competence = competences[i];
-                        arbre = arbre + ('  <competence>\n' +
-                        '       <id>' + competence.idCompetence + '</id>\n' +
-                        '       <nom>' + competence.nomCompetence + '</nom>\n' +
-                        '       <idPere>' + competence.idPereCompetence + '</idPere>\n');
-                    }
-                    arbre = arbre + '</arbre>';
-                }).fail( function(competences, textStatus, error){
-                    console.error("getJSON failed, status: " + textStatus + ", error: "+error)
-                });
-    document.getElementById('linkXML').onclick = function(code) {
-        this.href = 'data:application/xml;charset=utf-8,'
-        + encodeURIComponent(arbre);
+    for(var i=0, len = competencesValidees.length; i < len; ++i){
+        var competence = competencesValidees[i];
+        arbre += (  '  <competence>\n' +
+                    '       <id>' + competence.idCompetence + '</id>\n' +
+                    '       <nom>' + competence.nomCompetence + '</nom>\n' +
+                    '       <idPere>' + competence.idPereCompetence + '</idPere>\n');
+    }
+    arbre += '</arbre>';
+    $('#linkXML').onclick = function(){
+        this.href = 'data:application/xml;charset=utf-8,' + encodeURIComponent(arbre);
     };
-    console.log(arbre)
-};*/
+};
